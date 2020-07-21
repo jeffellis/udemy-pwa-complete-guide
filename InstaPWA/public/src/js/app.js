abe8fc2ba1cdbd5951951682b6bf0b019cb97368
Jeff Ellis <jeff@ellises.us>
2020-07-21 11:37:13 -0400

    Unzip section starting point

--- InstaPWA/public/src/js/app.js
+++ InstaPWA/public/src/js/app.js
@@ -1,27 +1,24 @@
-// Apply polyfills if needed
-if (!window.Promise) {
-    window.Promise = Promise;
-}
-if (!window.fetch) {
-    window.fetch = fetch;
-}
 
-// Are service workers supported?
-if ('serviceWorker' in navigator) {
-
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then(function() {
-            console.log('SW Registered');
-        });
-
-}
-
-// Defer install on home screen banner
 var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
 window.addEventListener('beforeinstallprompt', function(event) {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
