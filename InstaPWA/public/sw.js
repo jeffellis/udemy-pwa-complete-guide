98e9e16adfb44ba33957ac2448f18e14e215434b
Jeff Ellis <jeff@ellises.us>
2020-07-21 13:52:03 -0400

    - Pre-cache the app shell assets

--- InstaPWA/public/sw.js
+++ InstaPWA/public/sw.js
@@ -4,8 +4,33 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
       caches.open('static')
       .then(function(cache) {
-          console.log('[Service Worker]: Pre-caching app shell');
-          cache.add('/src/js/app.js');
+        console.log('[Service Worker]: Pre-caching app shell!');
+
+        // NOTE: Requests are stored exactly. Make sure to cache '/' 
+        cache.addAll([
+          '/',
+          '/manifest.json',
+          '/index.html',
+          '/src/js/app.js',
+          '/src/js/feed.js',
+          '/src/js/material.min.js',
+          '/src/css/app.css',
+          '/src/css/feed.css',
+          '/src/images/main-image.jpg',
+          'https://fonts.googleapis.com/css?family=Roboto:400,700',
+          'https://fonts.googleapis.com/icon?family=Material+Icons',
+          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+
+          // No real reason to store these polyfills since they are only 
+          // needed for browsers that do not support service workers. There is
+          // a slight perf improvement for caching them since all browsers have to
+          // load them.
+          '/src/js/promise.js',
+          '/src/js/fetch.js'
+
+          // No need to cache icons since we probably don't care about A2HS when
+          // offline
+        ]);
       })
   );
 });
@@ -16,5 +41,13 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+  // Return cached content if available
+  event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+            return response;
+        }
+      })
+  );
 });
